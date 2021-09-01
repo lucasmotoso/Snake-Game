cdd614bc186685d3f226e2ec768e5e9aa6b29a37
lucasmotoso <lucasmotoso@gmail.com>
2021-09-01 02:44:36 -0300

    Criado comida

--- script.js
+++ script.js
@@ -7,7 +7,10 @@ snake[0] = {
     y: 8* box,
 }
 let direction = "right";
-
+let food = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box,
+}
 
 
 function criarBG() {
@@ -20,6 +23,11 @@ function criarCobrinha(){
         context.fillStyle = "black";
         context.fillRect(snake[i].x, snake[i].y, box, box);
     }
+}
+
+function drawFood(){
+    context.fillStyle ="red";
+    context.fillRect(food.x, food.y, box, box);
 }
 
 document.addEventListener('keydown', update);
@@ -42,6 +50,7 @@ function iniciarJogo(){
 
     criarBG();
     criarCobrinha();
+    drawFood();
 
     let snakeX = snake[0].x; 
     let snakeY = snake[0].y;

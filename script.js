d0386f7dd1b66230b525a6337604f492c7535c2a
lucasmotoso <lucasmotoso@gmail.com>
2021-09-01 02:13:17 -0300

    Definir movimentos da snake

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ snake[0] = {
     x: 8* box,
     y: 8* box,
 }
+let direction = "right";
+
+
 
 function criarBG() {
     context.fillStyle = "#C8DA2E";
@@ -19,5 +22,26 @@ function criarCobrinha(){
     }
 }
 
-criarBG();
-criarCobrinha();
+function iniciarJogo(){
+    criarBG();
+    criarCobrinha();
+
+    let snakeX = snake[0].x; 
+    let snakeY = snake[0].y;
+
+    if(direction == "right") snakeX += box;
+    if(direction == "left") snakeX -= box;
+    if(direction == "up") snakeY -= box;
+    if(direction == "down") snakeY +=box;
+
+    snake.pop();
+
+    let newHead = {
+        x: snakeX,
+        y: snakeY,
+    }
+    snake.unshift(newHead);
+
+}
+
+let jogo = setInterval(iniciarJogo, 100);

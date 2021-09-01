434363650288e6b0fd7bb852027ccf75ee4fc768
lucasmotoso <lucasmotoso@gmail.com>
2021-09-01 03:18:55 -0300

    Definido fim do jogo

--- script.js
+++ script.js
@@ -46,7 +46,12 @@ function iniciarJogo(){
     if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
     if(snake[0].y < 0 * box && direction == "up") snake[0].y = 16 * box;
     
-
+    for (i = 1; i <snake.length; i++){
+        if (snake[0].x == snake[i].x && snake[0].y == snake [i].y){
+            clearInterval(jogo);
+            alert('Game Over :(  Aperte F5 para jogar novamente!')
+        }
+    }
 
     criarBG();
     criarCobrinha();
